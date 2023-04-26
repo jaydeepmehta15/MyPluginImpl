374ba04d5b056e486cc3bfffd97e9718f4d13b7b
Jaydeep Mehta <jmehta@simeio.com>
2023-04-27 05:28:04 +0530

    My Plugin Impl https://github.com/jaydeepmehta15/InterfaceExtensability

---
+++ src/com/Name/MyClass.java
@@ -0,0 +1,11 @@
+package com.Name;
+
+public class MyClass implements MyContract{
+
+	@Override
+	public void doSomething() {
+		System.out.println("I did something");
+		
+	}
+
+}
---
+++ src/com/Name/MyContract.java
@@ -0,0 +1,5 @@
+package com.Name;
+public interface MyContract {
+	
+	public void doSomething();
+}
